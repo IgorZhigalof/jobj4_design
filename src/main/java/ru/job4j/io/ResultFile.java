d60ad99b45d644e901a53020649350debbfd2947
Игорь <hardplayer455@gmail.com>
2023-11-18 21:41:54 +0300

    2.0. FileOutputStream [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        String ln = System.lineSeparator();
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            out.write(("1 * 1 = 1" + ln
+                        + "1 * 2 = 2" + ln
+                        + "1 * 3 = 3" + ln
+                        + "1 * 4 = 4" + ln
+                        + "1 * 5 = 5" + ln
+                        + "1 * 6 = 6" + ln
+                        + "1 * 7 = 7" + ln
+                        + "1 * 8 = 8" + ln
+                        + "1 * 9 = 9"
+                    ).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
