dbc7b7674933a758fd837d5d42502a9e5b3ad274
Игорь <hardplayer455@gmail.com>
2023-12-28 10:30:18 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,14 +5,10 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Map<FileProperty, Path> files = new HashMap<>();
-    private Set<FileProperty> duplicatedFiles = new HashSet<>();
+    private Map<FileProperty, List<Path>> files = new HashMap<>();
 
     @Override
     public FileVisitResult visitFile(Path file,
@@ -20,16 +16,18 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
         FileProperty property = new FileProperty(
                 file.toFile().getUsableSpace(),
                 file.toFile().getName());
-        if (duplicatedFiles.contains(property)) {
-            System.out.println(file.toAbsolutePath());
-        } else if (files.containsKey(property)) {
-            duplicatedFiles.add(property);
-            System.out.println(file.toAbsolutePath());
-            System.out.println(files.get(property));
-            files.remove(property);
+        if (files.containsKey(property)) {
+            files.get(property).add(file.toAbsolutePath());
         } else {
-            files.put(property, file.toAbsolutePath());
+            files.put(property, new ArrayList<>(List.of(file.toAbsolutePath())));
         }
         return super.visitFile(file, attributes);
+    }
+
+    public void printAllDuplicates() {
+        files.keySet().stream()
+                .filter(x -> files.get(x).size() > 1)
+                .flatMap(x -> files.get(x).stream())
+                .forEach(System.out::println);
     }
 }
